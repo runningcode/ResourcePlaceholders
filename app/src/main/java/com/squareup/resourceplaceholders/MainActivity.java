01e8c0e959dcdd47b2f76a2f2e7eb2941c9d55b2
Nelson Osacky <no@squareup.com>
2017-05-05 17:51:19 -0700

    Initial commit
    `./gradlew app:installDebug prints`
    `D/TAG: string is My Library`
    
    `./buckw install -r app:bin_debug`
    `D/TAG: string is `

---
+++ app/src/main/java/com/squareup/resourceplaceholders/MainActivity.java
@@ -0,0 +1,14 @@
+package com.squareup.resourceplaceholders;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Log.d("TAG", "string is " + getString(R.string.placeholder));
+  }
+}
